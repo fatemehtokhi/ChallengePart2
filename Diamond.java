3124934029a53da25b73948e9b3eba40f42181cd
fatemeh <fatemeh.rt@gmail.com>
2020-04-01 13:53:13 +0200

    Diamond oder pattern star printing

--- Diamond.java
+++ Diamond.java
@@ -1,21 +1,35 @@
 import java.util.Scanner;
 
-public class Diamond{
-    public static String print(int n){
+public class Diamond {
+    public static String print(int n) {
         String s = String.valueOf(n);
-        for(int i=0;i<n;i++){
-            System.out.println("*");
-            for(int j=0;j<n;j++){
-                System.out.println("*");
+        for (int i = 0; i <n; i++) {
+            for (int j = n-i; j > 1; j--) {
+                System.out.print(" ");
             }
+            for (int j = 0 ; j <= i ; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
         }
+        for (int i = n-1; i >= 1; i--) {
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 1 ; j<=i ; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
         return s;
     }
+
     public static void main(String[] args) {
         System.out.println("Please Enter a number:");
-        Scanner scan =new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         int rows = scan.nextInt();
         System.out.println(print(rows));
-        
+
     }
 }

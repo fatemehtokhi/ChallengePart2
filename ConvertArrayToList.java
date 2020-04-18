ef277b7feebbebf7f5a8c8584c01c5f037f127da
fatemeh <fatemeh.rt@gmail.com>
2020-04-18 07:39:14 +0200

    ConvertArrayToList

---
+++ ConvertArrayToList.java
@@ -0,0 +1,12 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+public class ConvertArrayToList {
+    public static void main(String[] args) {
+        Integer[] array = {23,24};
+        List <Integer> list = Arrays.asList(array);
+        System.out.println(list);
+        
+    }
+
+}

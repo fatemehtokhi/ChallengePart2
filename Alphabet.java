4445a7cbb6f9df6d34dd94fad2cceda63fc9d3a9
fatemeh <fatemeh.rt@gmail.com>
2020-05-22 18:17:21 +0200

    Alphabet

---
+++ Alphabet.java
@@ -0,0 +1,12 @@
+
+public class Alphabet {
+    public static void main(String[] args) {
+        char c = '^';
+        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+            System.out.println(c + " = This character is  Alphabet.");
+        } else {
+            System.out.println(c + " = This character is not Alphabet.");
+        }
+
+    }
+}
